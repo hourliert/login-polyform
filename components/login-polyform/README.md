7b577d42c82a46805df5c43df418079c0ae8598a
Thomas Hourlier <thomas.hourlier@cnode.fr>
2015-06-03 21:13:15 -0700

    seed gh-pages

---
+++ components/login-polyform/README.md
@@ -0,0 +1,12 @@
+# login-form
+
+A material login form asking for login and password.
+
+# Demo
+
+You can see a live version [here](http://hourliert.github.io/login-form/components/login-form/demo/)
+and browse the doc [here](http://hourliert.github.io/login-form/components/login-form/)
+
+#Install
+To install this component: 
+`bower install hourliert/login-form`
---
+++ components/login-polyform/gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp'),
+	bs = require('browser-sync').create(),
+	bg = require("gulp-bg"),
+	ps = require('ps-node');
+
+var POLYSERVE_PORT = 8080,
+    elementName = 'login-polyform';
+
+var browserSyncConfig = function(path, cb) {
+  bs.init({
+    proxy: 'localhost:' + POLYSERVE_PORT,
+    startPath: path
+  });
+
+  bs.reload;
+
+  process.on('exit', function() {
+    bs.exit();
+  });
+};
+
+var watchComponent = function() {
+  gulp.watch(['./*.html', 'demo/**/*.html', 'test/**/*.html'], bs.reload);
+};
+
+gulp.task('polyserve', function(cb) {
+  ps.lookup({
+    command: 'polyserve',
+    psargs: '-f'
+  }, function(err, resultList) {
+    if (err) {
+      throw new Error(err);
+    }
+ 
+    if (!resultList.length) {
+      bg('polyserve', '-p ' + POLYSERVE_PORT)();
+    }
+    cb();
+  }); 
+});
+	
+gulp.task('serve', ['polyserve'], function(cb) {
+  browserSyncConfig('/components/' + elementName + '/demo/', cb);
+  watchComponent();
+});
+
+gulp.task('serve:doc', ['polyserve'], function(cb) {
+  browserSyncConfig('/components/' + elementName + '/', cb);
+  watchComponent();
+});
+
+gulp.task('test:watch', ['polyserve'], function(cb) {
+  browserSyncConfig('/components/' + elementName + '/test/', cb);
+  watchComponent();
+});
